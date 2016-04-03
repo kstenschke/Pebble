9c1ef3cab380b9b8196e0a23133bbff66ce9c00e
Kay Stenschke <info@stenschke.com>
2016-04-03 11:17:32 +0200

    Update main.c
    removed gap between line sements

--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
 #include <pebble.h>
 
+#define PI 3.14159265
+
 Window *my_window;
 static Layer *canvas_layer;
+GRect bounds;
 bool has_time = false;
 bool dots_on = true;
 static char buffer[] = "00:00";
@@ -12,9 +15,11 @@ void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 }
 
 static int draw_dots(GContext *ctx, int x, int y, bool blink) {
-  dots_on = !dots_on;
-  
   if(!blink || dots_on) {
+    dots_on = !dots_on;
+    
+    graphics_context_set_fill_color(ctx, GColorWhite);
+    
     uint16_t radius = 2;
     
     GPoint center = GPoint(x+4, y+9);
@@ -28,18 +33,16 @@ static int draw_dots(GContext *ctx, int x, int y, bool blink) {
 }
 
 static int draw_one(GContext *ctx, int x, int y) {
-  graphics_fill_rect(ctx, GRect(x+12, y, 4, 16), 0, GCornerNone);
-  graphics_fill_rect(ctx, GRect(x+12, y+17, 4, 16), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(x+12, y-1, 4, 35), 0, GCornerNone);
   
   return x+16;
 }
 
 static int draw_zero(GContext *ctx, int x, int y) {
-  graphics_fill_rect(ctx, GRect(x, y, 4, 16), 0, GCornerNone);
-  graphics_fill_rect(ctx, GRect(x, y+17, 4, 16), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(x, y, 4, 32), 0, GCornerNone);
   
-  graphics_fill_rect(ctx, GRect(x+5, y-1, 6, 4), 0, GCornerNone);
-  graphics_fill_rect(ctx, GRect(x+5, y+30, 6, 4), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(x, y-1, 16, 4), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(x, y+30, 16, 4), 0, GCornerNone);
   
   draw_one(ctx, x, y);
   
@@ -49,9 +52,9 @@ static int draw_zero(GContext *ctx, int x, int y) {
 static int draw_two(GContext *ctx, int x, int y) {
   graphics_fill_rect(ctx, GRect(x, y+17, 4, 16), 0, GCornerNone);
   
-  graphics_fill_rect(ctx, GRect(x+5, y-1, 6, 4), 0, GCornerNone);
-  graphics_fill_rect(ctx, GRect(x+5, y+14, 6, 4), 0, GCornerNone);  
-  graphics_fill_rect(ctx, GRect(x+5, y+30, 6, 4), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(x, y-1, 16, 4), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(x, y+14, 16, 4), 0, GCornerNone);  
+  graphics_fill_rect(ctx, GRect(x, y+30, 16, 4), 0, GCornerNone);
   
   graphics_fill_rect(ctx, GRect(x+12, y, 4, 16), 0, GCornerNone);
   
@@ -59,9 +62,9 @@ static int draw_two(GContext *ctx, int x, int y) {
 }
 
 static int draw_three(GContext *ctx, int x, int y) {
-  graphics_fill_rect(ctx, GRect(x+5, y-1, 6, 4), 0, GCornerNone);
-  graphics_fill_rect(ctx, GRect(x+5, y+14, 6, 4), 0, GCornerNone);  
-  graphics_fill_rect(ctx, GRect(x+5, y+30, 6, 4), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(x, y-1, 16, 4), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(x, y+14, 16, 4), 0, GCornerNone);  
+  graphics_fill_rect(ctx, GRect(x, y+30, 16, 4), 0, GCornerNone);
   
   draw_one(ctx, x, y);
   
@@ -70,7 +73,7 @@ static int draw_three(GContext *ctx, int x, int y) {
 
 static int draw_four(GContext *ctx, int x, int y) {
   graphics_fill_rect(ctx, GRect(x, y, 4, 16), 0, GCornerNone);
-  graphics_fill_rect(ctx, GRect(x+5, y+14, 6, 4), 0, GCornerNone);  
+  graphics_fill_rect(ctx, GRect(x, y+14, 16, 4), 0, GCornerNone);  
   
   draw_one(ctx, x, y);
   
@@ -80,9 +83,9 @@ static int draw_four(GContext *ctx, int x, int y) {
 static int draw_five(GContext *ctx, int x, int y) {
   graphics_fill_rect(ctx, GRect(x, y, 4, 16), 0, GCornerNone);
   
-  graphics_fill_rect(ctx, GRect(x+5, y-1, 6, 4), 0, GCornerNone);
-  graphics_fill_rect(ctx, GRect(x+5, y+14, 6, 4), 0, GCornerNone);  
-  graphics_fill_rect(ctx, GRect(x+5, y+30, 6, 4), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(x, y-1, 16, 4), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(x, y+14, 16, 4), 0, GCornerNone);  
+  graphics_fill_rect(ctx, GRect(x, y+30, 16, 4), 0, GCornerNone);
   
   graphics_fill_rect(ctx, GRect(x+12, y+17, 4, 16), 0, GCornerNone);
   
@@ -93,9 +96,9 @@ static int draw_six(GContext *ctx, int x, int y) {
   graphics_fill_rect(ctx, GRect(x, y, 4, 16), 0, GCornerNone);
   graphics_fill_rect(ctx, GRect(x, y+17, 4, 16), 0, GCornerNone);
   
-  graphics_fill_rect(ctx, GRect(x+5, y-1, 6, 4), 0, GCornerNone);
-  graphics_fill_rect(ctx, GRect(x+5, y+14, 6, 4), 0, GCornerNone);  
-  graphics_fill_rect(ctx, GRect(x+5, y+30, 6, 4), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(x, y-1, 16, 4), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(x, y+14, 16, 4), 0, GCornerNone);  
+  graphics_fill_rect(ctx, GRect(x, y+30, 16, 4), 0, GCornerNone);
   
   graphics_fill_rect(ctx, GRect(x+12, y+17, 4, 16), 0, GCornerNone);
   
@@ -103,7 +106,7 @@ static int draw_six(GContext *ctx, int x, int y) {
 }
 
 static int draw_seven(GContext *ctx, int x, int y) {
-  graphics_fill_rect(ctx, GRect(x+5, y-1, 6, 4), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(x, y-1, 16, 4), 0, GCornerNone);
   
   draw_one(ctx, x, y);
   
@@ -111,7 +114,7 @@ static int draw_seven(GContext *ctx, int x, int y) {
 }
 
 static int draw_eight(GContext *ctx, int x, int y) {
-  graphics_fill_rect(ctx, GRect(x+5, y+14, 6, 4), 0, GCornerNone);  
+  graphics_fill_rect(ctx, GRect(x, y+14, 16, 4), 0, GCornerNone);  
   
   draw_zero(ctx, x, y);
   
@@ -124,6 +127,12 @@ static int draw_nine(GContext *ctx, int x, int y) {
   draw_three(ctx, x, y);
   
   return x+16;
+}
+
+static void draw_text(GContext *ctx, int x, int y) {
+//  graphics_fill_rect(ctx, GRect(0, y-12, 144, 1), 0, GCornerNone);
+  
+  
 }
 
 static int draw_number(char c, GContext *ctx, int x, int y) {
@@ -153,12 +162,12 @@ static int draw_number(char c, GContext *ctx, int x, int y) {
 
 static void canvas_update_proc(Layer *layer, GContext *ctx) {
   if(has_time) {
+    graphics_context_set_text_color(ctx, GColorBlack);
     graphics_context_set_fill_color(ctx, GColorWhite);
     
     int offset = 5;
-    int x = 25;
-    int y = 20;
-
+    int x = 26;
+    int y = 60;
     
     x = draw_number(buffer[0], ctx, x, y);
     x = draw_number(buffer[1], ctx, x+offset, y);
@@ -168,7 +177,7 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
   }
 }
 
-static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
+static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
   strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
   has_time = true;
   layer_mark_dirty(canvas_layer);
@@ -179,7 +188,7 @@ void handle_init(void) {
   window_set_background_color(my_window, GColorBlack);
 
   // Create canvas layer
-  GRect bounds = layer_get_bounds(window_get_root_layer(my_window));
+  bounds = layer_get_bounds(window_get_root_layer(my_window));
   canvas_layer = layer_create(bounds);
 
   // Assign the custom drawing procedure
@@ -190,7 +199,7 @@ void handle_init(void) {
   window_stack_push(my_window, true);
   
   // Add update per minute
-  tick_timer_service_subscribe(SECOND_UNIT, (TickHandler)handle_minute_tick);
+  tick_timer_service_subscribe(SECOND_UNIT, (TickHandler)handle_tick);
 }
 
 void handle_deinit(void) {
